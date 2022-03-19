9675771d0c29bf4977398092746e53a3fa179422
CaioB99 <caio_b_s@hotmail.com>
2022-03-19 20:06:23 -0300

    Accordion adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import api from './services/api'
 import Header from './components/Header'
+import Content from './components/Content'
 
 export default function App() {
   return (
   <>
     <Header />
+    <div className="container">
+      <Content />
+    </div>
   </>
   )
 }
