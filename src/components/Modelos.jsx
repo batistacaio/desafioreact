7845d973bf630c9125cb6b4c02036dc7d74d71f8
CaioB99 <caio_b_s@hotmail.com>
2022-03-19 23:04:41 -0300

    modelos.map fixed, falta estilizar para finalização

--- src/components/Modelos.jsx
+++ src/components/Modelos.jsx
@@ -11,18 +11,22 @@ function Modelos() {
         fetch(`https://parallelum.com.br/fipe/api/v1/${window.location.pathname.split('/')[1]}/marcas/${id()}/modelos`)
             .then(res => res.json())
             .then(data => setModelos(data))
+            .catch(err => console.log(err))
     }, [])
 
     return(
-        <ul>
-            <>
-            {modelos.map(modelo => (
-                <li key={modelo.codigo}>
-                    <p>{modelo.codigo} - {modelo.nome}</p>
-                </li>
-            ))}
-            </>
-        </ul>
+        <>
+        <div className="container">
+            <ul>
+                {modelos.modelos?.map(modelo => (
+                    <li key={modelo.codigo}>
+                        <p>{modelo.codigo} - {modelo.nome}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <button className="btn btn-primary" onClick={() => window.history.back()}>Voltar</button>
+        </>
     )
 }
 
