d6d2d83c61c23274552e7519ead97de2db33c65f
CaioB99 <caio_b_s@hotmail.com>
2022-03-20 15:14:09 -0300

    Filtro adicionado em modelos

--- src/components/Modelos.jsx
+++ src/components/Modelos.jsx
@@ -18,11 +18,15 @@ function Modelos() {
         <>
         <div className="container">
             <ul>
-                {(modelos.modelos?.map(modelo => (              
+                {modelos.modelos?.sort((a, b) => {
+                    if(a.nome < b.nome) return 1
+                    if(a.nome > b.nome) return -1
+                    return 0
+                }).map(modelo => (
                     <li key={modelo.codigo}>
                         <p>{modelo.codigo} - {modelo.nome}</p>
                     </li>
-                )))}
+                ))}
             </ul>
         </div>
         <button className="btn btn-primary" onClick={() => window.history.back()}>Voltar</button>
