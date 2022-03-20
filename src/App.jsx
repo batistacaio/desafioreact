34f2de89d8209660657c8d517c1c2e5ea0fc84cb
CaioB99 <caio_b_s@hotmail.com>
2022-03-19 21:25:53 -0300

    Adicionado consumo API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import api from './services/api'
 import Header from './components/Header'
 import Content from './components/Content'
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,43 +1,66 @@
+import { useState, useEffect } from 'react'
 import { Accordion } from 'react-bootstrap'
 import "../App.css"
 
 function Content() {
+    const [marcasCarro, setMarcasCarro] = useState([])
+    const [marcasMoto, setMarcasMoto] = useState([])
+    const [marcasCaminhao, setMarcasCaminhao] = useState([])
+
+    useEffect(() => {
+        fetch("https://parallelum.com.br/fipe/api/v1/carros/marcas")
+            .then(res => res.json())
+            .then(data => setMarcasCarro(data))
+    }, [])
+
+    useEffect(() => {
+        fetch("https://parallelum.com.br/fipe/api/v1/motos/marcas")
+            .then(res => res.json())
+            .then(data => setMarcasMoto(data))
+    }, [])
+
+    useEffect(() => {
+        fetch("https://parallelum.com.br/fipe/api/v1/caminhoes/marcas")
+            .then(res => res.json())
+            .then(data => setMarcasCaminhao(data))
+    }, [])
+
     return(
         <Accordion defaultActiveKey={['0']} alwaysOpen>
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Carros</Accordion.Header>
                 <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                    <ul>
+                        {marcasCarro.map(marca => (
+                            <li key={marca.codigo}>
+                                <a href={`/carros/${marca.codigo}`}>{marca.nome}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Motos</Accordion.Header>
                 <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                    <ul>
+                        {marcasMoto.map(marca => (
+                            <li key={marca.codigo}>
+                                <a href={`/motos/${marca.codigo}`}>{marca.nome}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
                 <Accordion.Header>Caminhões</Accordion.Header>
                 <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                    <ul>
+                        {marcasCaminhao.map(marca => (
+                            <li key={marca.codigo}>
+                                <a href={`/caminhoes/${marca.codigo}`}>{marca.nome}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
